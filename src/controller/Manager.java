8d8eaf4f4419cba6c18d34041e979a4e4fa66efd
Saïd Mezhoud <said.mezhoud@voidmx.net>
2016-12-13 23:53:56 +0100

    v1.2 - Init EJB to oreillyDS

--- src/controller/Manager.java
+++ src/controller/Manager.java
@@ -1,17 +1,24 @@
 package controller;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.Vector;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.naming.remote.client.InitialContextFactory;
 
 import model.Livre;
 import service.LivrePOJO;
 import storage.Dao;
 import storage.DaoJPA;
 import technique.LivreManager;
+import oreilly.DaoJPARemote;
 
 public class Manager {
 	private static Manager instance = new Manager();
-	private Dao<?> dao = new DaoJPA<LivrePOJO>();
 	public List<LivreManager> lesLivres;
 	public Livre monLivre;
 	
@@ -32,8 +39,14 @@ public class Manager {
 	
 	public void init() {
 		Dao<LivrePOJO> dao = null;
-		dao = (Dao<LivrePOJO>) this.dao;
-		
+		dao = (Dao<LivrePOJO>) dao;
+
+		ajouterLivres(dao);
+		readDaoEJB();
+
+	}
+	
+	public void ajouterLivres(Dao<LivrePOJO> dao) {
 		List<LivrePOJO> tmp = null;
 		tmp = (List<LivrePOJO>) dao.selectAll();
 		for(LivrePOJO lp : tmp)
@@ -46,5 +59,23 @@ public class Manager {
 		return lesLivres;
 	}
 	
+	private void readDaoEJB() {
+		DaoJPARemote dao = null;
+		InitialContext contexte = null;
+		Properties env = null;
+		
+		try {
+			env = new Properties();
+		    env.put("jboss.naming.client.ejb.context", true); 
+		    env.put(Context.INITIAL_CONTEXT_FACTORY, InitialContextFactory.class.getName());
+			env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+			contexte = new InitialContext(env);
+			dao = (DaoJPARemote) contexte.lookup("//oreillyDS/DaoJPA!oreilly.DaoJPARemote");
+			ajouterLivres(dao);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
